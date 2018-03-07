1362e4d95cc9f881e83ae5aa4006763419b7b4b0
SULAIMAN <SULAIMAN@DESKTOP-IUTC69S>
2018-03-07 09:37:27 -0800

    Add project files.

---
+++ BunyanApp/BunyanApp/CompanyDetails.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace BunyanApp
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class CompanyDetails : ContentPage
+	{
+		public CompanyDetails ()
+		{
+			InitializeComponent ();
+		}
+
+        private void mySlider_ValueChanged(object sender , ValueChangedEventArgs e)
+        {
+            txtResult.Text = "Value: " + mySlider.Value;
+        }
+
+    }
+}
